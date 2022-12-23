5c874dc5bdac0fa2cdc682f5cafe887646b8d3c3
Coder Gautam <gautamgxtv@gmail.com>
2022-12-23 14:44:50 -0600

    Basic Pathway System

--- components/Sets.js
+++ components/Sets.js
@@ -1,9 +1,8 @@
 import styled from "styled-components"
 import useSWR from "swr"
-import Link from "next/link"
 import { useRouter } from "next/router"
+import SetCard from "./SetCard"
 
-import Underline from "@/components/Underline"
 
 // use CSS grid to create a 3x2 grid of sets
 const SetGroup = styled.div`
@@ -18,38 +17,7 @@ const SetGroup = styled.div`
     flex-direction: column;
   }
 `
-const Set = styled.div`
-  cursor: pointer;
-  height: 8em;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 15px;
-  background-color: rgba(255, 156, 251, 0.2);
-`
-const SetTitle = styled(Link)`
-  text-decoration: none;
-  color: var(--color-gray);
-  font-size: 1.3rem;
-`
-const SetInfo = styled.div`
-  display: flex;
-  justify-content: space-between;
-`
-const SetCreator = styled(Link)`
-  text-decoration: none;
-  color: #db7bd8;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-  max-width: 40%;
-`
-const SetQuestions = styled.span`
-  color: #737373;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-`
+
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
@@ -68,27 +36,7 @@ export default function Sets() {
   return (
     <SetGroup>
       {data.map(set => (
-        <Set key={set.id} onClick={e => {
-          // if the user is trying to click on the creator of the set
-          // don't take them to the actual set
-          if(e.target.tagName !== "A")
-            router.push("/sets/" + set.id);
-        }}>
-          <SetTitle href={"/sets/" + set.id}>
-            <Underline
-              styles={
-                `transition: background-size 300ms;
-                :hover {
-                  background-size: 100% 10px;
-                }`
-              }
-            >{set.name}</Underline>
-          </SetTitle>
-          <SetInfo>
-            <SetCreator href={"/user/" + set.user}>{set.user}</SetCreator>
-            <SetQuestions>{set.questions.length} question{set.questions.length - 1 ? "s" : ""}</SetQuestions>
-          </SetInfo>
-        </Set>
+        <SetCard set={set} key={set.id} />
         )
       )}
     </SetGroup>
--- components/page/learn/Pathway.js
+++ components/page/learn/Pathway.js
@@ -1,7 +1,69 @@
-export default function Pathway() {
-  return (
-    <div style={{textAlign: "center"}}>
-      <h1>Pathway</h1>
-    </div>
+import styled from "styled-components"
+import useSWR from "swr"
+import { useRouter } from "next/router"
+import SetCard from "@/components/SetCard"
+import {pathway} from "../../pathway.json"
+
+
+// use CSS grid to create a 3x2 grid of sets
+const SetGroup = styled.div`
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 15px;
+  grid-row-gap: 15px;
+  margin: 0 auto;
+  max-width: 500px;
+
+    display: flex;
+    flex-direction: column;
+`
+
+const UnitCard = styled.div`
+  background-color: #f5f5f5;
+  border-radius: 10px;
+  padding: 10px;
+  margin-bottom: 10px;
+  
+`
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+export default function Sets() {
+  const router = useRouter()
+  const { data, error } = useSWR("/api/sets/pathway", fetcher)
+  console.log(data)
+
+  if (error) {
+    console.error(error);
+    return <div>An unknown error occured...</div>
+  }
+  if (!data) return (
+    <p>Loading...</p>
   )
+  return (
+    <center>
+    <SetGroup>
+    {
+      data.map((unit,i) => {
+
+        return (
+          <div>
+            <UnitCard>
+          <h1>{pathway[i].name}</h1>
+          <p>{pathway[i].description}</p>
+          </UnitCard>
+          {
+            unit.map(set => {
+              return (
+                <SetCard set={set} key={set.id} />
+              )
+            })
+          }
+          </div>
+        )
+
+      })
+    }
+    </SetGroup>
+    </center>
+  );
 }
