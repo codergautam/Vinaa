d87a6d597c38765287cee2db11ec5a1bbaa10e18
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2023-01-14 20:58:08 +0000

    Styling fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,20 @@
 import { SessionProvider } from "next-auth/react"
 import { Toaster } from "react-hot-toast"
 import GlobalStyles from "@/components/globalStyles"
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
+    <>
+    < GoogleAnalytics trackPageViews />
     <SessionProvider session={session}>
       <Toaster />
       <GlobalStyles />
       <Component {...pageProps} />
     </SessionProvider>
+    </>
   )
 }
